854f3677bf3516b47c6c86af3323cf4bad9b8f20
Eugene Triguba <eugenetriguba@gmail.com>
2020-01-18 20:32:32 -0600

    Resolve merge conflict

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,7 +7,11 @@ import (
 )
 
 func TestLetStatements(t *testing.T) {
-	input := `let x = 5; let y = 10; let foobar = 838383; `
+	input := `
+let x 5; 
+let y = 10; 
+let foobar = 838383;
+`
 
 	l := lexer.New(input)
 	parser := New(l)
