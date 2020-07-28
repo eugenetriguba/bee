b1e14a8e4decbe8aba2c00e1f9c2167b1a2ce230
Eugene Triguba <eugenetriguba@gmail.com>
2020-07-28 05:06:03 -0500

    Prepare AST for expressions

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,17 @@
 package ast
 
-import "bee/token"
+import (
+	"bytes"
+
+	"bee/token"
+)
 
 // Node is implemented by every node
-// in the AST. TokenLiteral() is only
-// for testing and debugging,
+// in the AST. TokenLiteral() and String()
+// are used for testing and debugging.
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement defines whether a node is
@@ -38,6 +43,16 @@ func (program *Program) TokenLiteral() string {
 	}
 }
 
+func (program *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range program.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement represents a `let` variable
 // declaration in the AST.
 type LetStatement struct {
@@ -46,10 +61,24 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode() {}
-
-// TokenLiteral retrieves the literal token (the string representation).
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // ReturnStatement represents a `return <expression>`
 // statement.
@@ -58,10 +87,43 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
-
-// TokenLiteral retrieves the literal token (the string representation).
+func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+
+}
+
+// ExpressionStatement a statement that consists of one expression.
+// In that sense, it is not really a statement, more of a wrapper.
+// We need it because it is completetly legal to write something like:
+// `let x = 5; x + 10;`
+type ExpressionStatement struct {
+	Token      token.Token // first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
 
 // Identifier represents a identifier in Bee
 // such as `let x = 5;`, where `x` is the identifier.
@@ -75,7 +137,6 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressionNode() {}
-
-// TokenLiteral retrieves the literal token (the string representation).
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
